b8fc6d2337efd5efec2f21503128ec2180af15a2
Jason Perr <trezero@gmail.com>
2023-11-03 14:23:30 -0700

    Updated so that every generation is unique, only need to change filename now for unique md5

--- createTestVideo.py
+++ createTestVideo.py
@@ -1,6 +1,10 @@
 import subprocess
+import time
 
-def generate_video_with_timecode(output_file="test_video_with_timecodev5.mov"):
+def generate_video_with_timecode(output_file="test_video_with_timecode.mov"):
+    # Get current timestamp for uniqueness
+    timestamp = str(time.time())
+    
     command = [
         "ffmpeg",
         "-f", "lavfi", "-i", "color=c=blue:s=1280x720:d=5",
@@ -8,8 +12,8 @@ def generate_video_with_timecode(output_file="test_video_with_timecodev5.mov"):
         "-f", "lavfi", "-i", "aevalsrc=0.5*sin(550*2*PI*t)|0.5*sin(550*2*PI*t):s=44100:d=5",
         "-f", "lavfi", "-i", "aevalsrc=0.5*sin(660*2*PI*t)|0.5*sin(660*2*PI*t):s=44100:d=5",
         "-f", "lavfi", "-i", "aevalsrc=0.5*sin(770*2*PI*t)|0.5*sin(770*2*PI*t):s=44100:d=5",
-        "-vf", "drawtext=text='Test Video':fontsize=24:fontcolor=white:x=(w-text_w)/2:y=(h-text_h)/2",
-        "-timecode", "00:01:53:00",
+        "-vf", f"drawtext=text='Test Video':fontsize=24:fontcolor=white:x=(w-text_w)/2:y=(h-text_h)/2,drawtext=text='{timestamp}':fontsize=0:fontcolor=white:x=(w-text_w)/2:y=(h-text_h)/2",
+        "-timecode", "00:01:57:00",
         "-metadata:s:v:0", "timecode=00:01:57:00",
         "-c:a", "aac", 
         "-strict", "experimental",
